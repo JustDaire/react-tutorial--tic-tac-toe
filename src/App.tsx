2b82771b081318b5ab9a186dbbc8d36ae8338cc8
JustDaire <11017338+JustDaire@users.noreply.github.com>
2023-08-14 10:10:23 +0000

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
 import './style.css';
 
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
   );
 }
